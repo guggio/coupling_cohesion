9c2f6155516419b5ff9f7d605782677894bd7135
guggio <sebastian.guggisberg@gmail.com>
2022-10-21 14:07:06 +0200

    initial commit

--- src/test/java/com/guggio/MainTest.java
+++ src/test/java/com/guggio/MainTest.java
@@ -1,4 +1,13 @@
+package com.guggio;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class MainTest {
-  
+
+  @Test
+  void testSetup() {
+    assertTrue(true);
+  }
 }
